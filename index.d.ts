024ddd4232423f20bdf239b41d2429758d68c166
dongy7 <dongy7@gmail.com>
2017-07-16 17:00:46 -0400

    updating types

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,18 @@
-export = Polyline
-export as namespace Polyline
+type Coordinate = [number, number]
+type Line = Coordinate[]
 
-declare namespace Polyline {
-  type Coordinate = [number, number]
-  type Line = Coordinate[]
-  class Polyline {
-    static getDistanceBetweenPoints(start: Coordinate, end: Coordinate): number
-    static getDistanceBetweenPointsKm(
-      start: Coordinate,
-      end: Coordinate
-    ): number
-    constructor(line: Line)
-    getLengthInMiles(): number
-    getLengthInKm(): number
-    getPointCovering(percent: number): Coordinate
-  }
+declare class Polyline {
+  getLengthInMiles(): number
+  getLengthInKm(): number
+  getPointCovering(percent: number): Coordinate
 }
+
+export declare function createLine(line: Line): Polyline
+export declare function getDistanceBetweenPoints(
+  start: Coordinate,
+  end: Coordinate
+): number
+export declare function getDistanceBetweenPointsKm(
+  start: Coordinate,
+  end: Coordinate
+): number
